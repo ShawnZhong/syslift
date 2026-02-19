e0d484f7cb6fb257b04a911add66281a0a4adc6f
ShawnZhong <github@shawnzhong.com>
2026-02-19 04:06:48 -0600

    Conver to IR pass

--- cc.sh
+++ cc.sh
@@ -13,8 +13,5 @@ plugin_path="${3:-build/libSysliftCollectSyscallsPass.so}"
 mkdir -p "$(dirname "$out_prefix")"
 
 clang -O2 -fno-unwind-tables -fno-asynchronous-unwind-tables \
-  -Ithird_party/nolibc -S -emit-llvm "$input_src" -o "${out_prefix}.ll"
-llc -stop-after=prologepilog "${out_prefix}.ll" -o "${out_prefix}.pre.mir"
-llc --load="$plugin_path" -run-pass=syslift-collect-syscalls "${out_prefix}.pre.mir" -o "${out_prefix}.mir"
-llc -start-after=prologepilog -filetype=obj "${out_prefix}.mir" -o "${out_prefix}.o"
-clang -nostdlib -static "${out_prefix}.o" -o "${out_prefix}"
+  -Ithird_party/nolibc -fpass-plugin="$plugin_path" \
+  -nostdlib -static "$input_src" -o "${out_prefix}"
--- pass/SysliftCollectSyscalls.cpp
+++ pass/SysliftCollectSyscalls.cpp
@@ -1,24 +1,14 @@
+#include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/Twine.h"
-#include "llvm/CodeGen/MachineFunction.h"
-#include "llvm/CodeGen/MachineFunctionPass.h"
-#include "llvm/CodeGen/MachineInstr.h"
-#include "llvm/CodeGen/MachineInstrBuilder.h"
-#include "llvm/CodeGen/MachinePassManager.h"
-#include "llvm/CodeGen/TargetInstrInfo.h"
-#include "llvm/CodeGen/TargetRegisterInfo.h"
-#include "llvm/CodeGen/TargetSubtargetInfo.h"
-#include "llvm/CodeGen/TargetOpcodes.h"
-#include "llvm/IR/Function.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/InlineAsm.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
-#include "llvm/MC/MCContext.h"
-#include "llvm/MC/MCSymbol.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
-#include "llvm/Pass.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Target/TargetMachine.h"
 #include "llvm/TargetParser/Triple.h"
 
 #include <atomic>
@@ -42,130 +32,61 @@ static cl::opt<std::string> SysliftSectionName(
 
 static std::atomic<uint64_t> GlobalSiteId{0};
 
-static bool isSvcOpcode(const MachineInstr &MI, const TargetInstrInfo *TII) {
-  return TII->getName(MI.getOpcode()) == "SVC";
+static bool containsSvcImmZero(StringRef AsmText) {
+  return AsmText.contains_insensitive("svc #0") ||
+         AsmText.contains_insensitive("svc\t#0") ||
+         AsmText.contains_insensitive("svc 0") ||
+         AsmText.contains_insensitive("svc\t0");
 }
 
-static bool isSvcInlineAsm(const MachineInstr &MI) {
-  if (!MI.isInlineAsm() || MI.getNumOperands() == 0) {
-    return false;
-  }
-
-  const MachineOperand &AsmOp = MI.getOperand(0);
-  if (!AsmOp.isSymbol()) {
-    return false;
-  }
-
-  StringRef AsmText = AsmOp.getSymbolName();
-  if (AsmText.contains_insensitive("svc #0")) {
-    return true;
-  }
-  if (AsmText.contains_insensitive("svc\t#0")) {
-    return true;
-  }
-  if (AsmText.contains_insensitive("svc 0")) {
-    return true;
-  }
-  if (AsmText.contains_insensitive("svc\t0")) {
-    return true;
-  }
-  return false;
+static bool isX8ConstraintCode(StringRef Code) {
+  return Code.equals_insensitive("{x8}") || Code.equals_insensitive("{w8}");
 }
 
-static bool isSvcImmZero(const MachineInstr &MI, const TargetInstrInfo *TII) {
-  if (isSvcOpcode(MI, TII)) {
-    for (const MachineOperand &Op : MI.operands()) {
-      if (!Op.isImm()) {
-        continue;
-      }
-      return Op.getImm() == 0;
-    }
-    return false;
-  }
-  return isSvcInlineAsm(MI);
-}
+static std::optional<unsigned>
+findX8InputArgIndex(const InlineAsm &IA) {
+  InlineAsm::ConstraintInfoVector Constraints = IA.ParseConstraints();
+  unsigned ArgIndex = 0;
 
-static int findOpcodeByName(const TargetInstrInfo *TII, StringRef Name) {
-  for (unsigned Opcode = 0; Opcode < TII->getNumOpcodes(); ++Opcode) {
-    if (TII->getName(Opcode) == Name) {
-      return static_cast<int>(Opcode);
-    }
-  }
-  return -1;
-}
-
-static Register findPhysRegByName(const TargetRegisterInfo *TRI,
-                                  StringRef Name) {
-  for (unsigned Reg = 1; Reg < TRI->getNumRegs(); ++Reg) {
-    StringRef RegName = TRI->getName(Reg);
-    if (RegName.equals_insensitive(Name)) {
-      return Register(Reg);
-    }
-  }
-  return Register();
-}
-
-static bool isRegNamed(Register Reg, const TargetRegisterInfo *TRI,
-                       StringRef Name) {
-  if (!Reg.isValid() || !Reg.isPhysical()) {
-    return false;
-  }
-  return StringRef(TRI->getName(Reg.id())).equals_insensitive(Name);
-}
-
-static bool isX8Reg(Register Reg, const TargetRegisterInfo *TRI) {
-  return isRegNamed(Reg, TRI, "x8") || isRegNamed(Reg, TRI, "w8");
-}
-
-static bool definesX8(const MachineInstr &MI, const TargetRegisterInfo *TRI) {
-  for (const MachineOperand &Op : MI.operands()) {
-    if (!Op.isReg() || !Op.isDef()) {
+  for (const InlineAsm::ConstraintInfo &Constraint : Constraints) {
+    if (!Constraint.hasArg()) {
       continue;
     }
-    if (isX8Reg(Op.getReg(), TRI)) {
-      return true;
+
+    const bool IsInput = Constraint.Type == InlineAsm::isInput;
+    if (IsInput) {
+      for (const std::string &Code : Constraint.Codes) {
+        if (isX8ConstraintCode(Code)) {
+          return ArgIndex;
+        }
+      }
     }
+
+    ++ArgIndex;
   }
-  return false;
+
+  return std::nullopt;
 }
 
-static std::optional<uint32_t>
-extractSysNrFromX8Def(const MachineInstr &MI, const TargetInstrInfo *TII) {
-  StringRef OpcodeName = TII->getName(MI.getOpcode());
-  if (OpcodeName != "MOVi32imm" && OpcodeName != "MOVi64imm") {
+static std::optional<uint32_t> extractSyscallNumber(const CallBase &CB,
+                                                    const InlineAsm &IA) {
+  std::optional<unsigned> X8ArgIdx = findX8InputArgIndex(IA);
+  if (!X8ArgIdx.has_value() || X8ArgIdx.value() >= CB.arg_size()) {
     return std::nullopt;
   }
 
-  for (const MachineOperand &Op : MI.operands()) {
-    if (!Op.isImm()) {
-      continue;
-    }
-    int64_t Imm = Op.getImm();
-    if (Imm < 0 || Imm > std::numeric_limits<uint32_t>::max()) {
-      return std::nullopt;
-    }
-    return static_cast<uint32_t>(Imm);
+  const Value *Arg = CB.getArgOperand(X8ArgIdx.value());
+  const auto *Imm = dyn_cast<ConstantInt>(Arg);
+  if (Imm == nullptr) {
+    return std::nullopt;
   }
-  return std::nullopt;
-}
-
-static std::optional<uint32_t>
-findSyscallNumber(const MachineBasicBlock &MBB,
-                  MachineBasicBlock::const_iterator SiteIt,
-                  const TargetInstrInfo *TII,
-                  const TargetRegisterInfo *TRI) {
-  for (auto It = SiteIt; It != MBB.begin();) {
-    --It;
-    const MachineInstr &MI = *It;
-    if (MI.isDebugInstr() || MI.isCFIInstruction()) {
-      continue;
-    }
-    if (!definesX8(MI, TRI)) {
-      continue;
-    }
-    return extractSysNrFromX8Def(MI, TII);
+  if (Imm->isNegative()) {
+    return std::nullopt;
   }
-  return std::nullopt;
+  if (Imm->getValue().ugt(std::numeric_limits<uint32_t>::max())) {
+    return std::nullopt;
+  }
+  return static_cast<uint32_t>(Imm->getZExtValue());
 }
 
 static bool isAlwaysAllowedSyscall(uint32_t SysNr) {
@@ -184,109 +105,86 @@ static std::string buildSectionEntryAsm(StringRef SectionName,
   return Asm;
 }
 
+static std::string buildPatchedSiteAsm(StringRef SiteLabel) {
+  std::string Asm;
+  raw_string_ostream OS(Asm);
+  OS << SiteLabel << ":\n";
+  OS << "\tmov x0, #" << AArch64NegEnosys << "\n";
+  return Asm;
+}
+
+static InlineAsm *getInlineAsmCallee(CallBase &CB) {
+  Value *Callee = CB.getCalledOperand();
+  while (auto *CE = dyn_cast<ConstantExpr>(Callee)) {
+    if (!CE->isCast()) {
+      break;
+    }
+    Callee = CE->getOperand(0);
+  }
+  return dyn_cast<InlineAsm>(Callee);
+}
+
 class SysliftCollectSyscallsPass
     : public PassInfoMixin<SysliftCollectSyscallsPass> {
 public:
-  static bool runImpl(MachineFunction &MF) {
-    const Triple &TT = MF.getTarget().getTargetTriple();
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
+    const Triple TT(M.getTargetTriple());
     if (!TT.isAArch64()) {
-      return false;
+      return PreservedAnalyses::all();
     }
     if (SysliftSectionName.empty()) {
-      return false;
+      return PreservedAnalyses::all();
     }
 
-    Module &M = *MF.getFunction().getParent();
-    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
-    const TargetRegisterInfo *TRI = MF.getSubtarget().getRegisterInfo();
-    int MovI64ImmOpcode = findOpcodeByName(TII, "MOVi64imm");
-    if (MovI64ImmOpcode < 0) {
-      report_fatal_error("SysliftCollectSyscallsPass: AArch64 MOVi64imm not found");
-    }
-    Register X0Reg = findPhysRegByName(TRI, "x0");
-    if (!X0Reg.isValid()) {
-      report_fatal_error("SysliftCollectSyscallsPass: AArch64 x0 register not found");
-    }
     bool Changed = false;
 
-    for (MachineBasicBlock &MBB : MF) {
-      for (MachineBasicBlock::iterator It = MBB.begin(); It != MBB.end();) {
-        MachineInstr &MI = *It;
-        MachineBasicBlock::iterator SiteIt = It;
-        ++It;
-        if (!isSvcImmZero(MI, TII)) {
-          continue;
+    for (Function &F : M) {
+      for (BasicBlock &BB : F) {
+        for (Instruction &I : BB) {
+          auto *CB = dyn_cast<CallBase>(&I);
+          if (CB == nullptr) {
+            continue;
+          }
+
+          InlineAsm *IA = getInlineAsmCallee(*CB);
+          if (IA == nullptr) {
+            continue;
+          }
+          if (!containsSvcImmZero(IA->getAsmString())) {
+            continue;
+          }
+
+          std::optional<uint32_t> SysNr = extractSyscallNumber(*CB, *IA);
+          if (!SysNr.has_value()) {
+            report_fatal_error(
+                Twine("SysliftCollectSyscallsPass: unable to prove constant x8 for "
+                      "syscall site in function ") +
+                F.getName());
+          }
+          if (isAlwaysAllowedSyscall(SysNr.value())) {
+            continue;
+          }
+
+          const uint64_t SiteId = GlobalSiteId.fetch_add(1, std::memory_order_relaxed);
+          const std::string SiteLabel =
+              (Twine("__syslift_syscall_site_") + Twine(SiteId)).str();
+
+          M.appendModuleInlineAsm(buildSectionEntryAsm(
+              SysliftSectionName, SiteLabel, SysNr.value()));
+
+          InlineAsm *Replacement = InlineAsm::get(
+              IA->getFunctionType(), buildPatchedSiteAsm(SiteLabel),
+              IA->getConstraintString(), IA->hasSideEffects(), IA->isAlignStack(),
+              IA->getDialect(), IA->canThrow());
+          CB->setCalledOperand(Replacement);
+          Changed = true;
         }
-
-        std::optional<uint32_t> SysNr =
-            findSyscallNumber(MBB, SiteIt, TII, TRI);
-        if (!SysNr.has_value()) {
-          report_fatal_error(
-              Twine("SysliftCollectSyscallsPass: unable to prove constant x8 for "
-                    "syscall site in function ") +
-              MF.getName());
-        }
-        if (isAlwaysAllowedSyscall(SysNr.value())) {
-          // Keep exit syscalls intact and out of policy instrumentation.
-          continue;
-        }
-
-        uint64_t SiteId = GlobalSiteId.fetch_add(1, std::memory_order_relaxed);
-        std::string SiteLabel =
-            (Twine("__syslift_syscall_site_") + Twine(SiteId)).str();
-        MCSymbol *Sym = MF.getContext().getOrCreateSymbol(SiteLabel);
-
-        BuildMI(MBB, SiteIt, MI.getDebugLoc(),
-                TII->get(TargetOpcode::ANNOTATION_LABEL))
-            .addSym(Sym);
-
-        M.appendModuleInlineAsm(buildSectionEntryAsm(
-            SysliftSectionName, SiteLabel, SysNr.value()));
-
-        if (isSvcInlineAsm(MI)) {
-          MI.getOperand(0).ChangeToES("mov x0, #-38");
-        } else {
-          BuildMI(MBB, SiteIt, MI.getDebugLoc(), TII->get(MovI64ImmOpcode),
-                  X0Reg)
-              .addImm(AArch64NegEnosys);
-          MI.eraseFromParent();
-        }
-
-        Changed = true;
       }
     }
 
-    return Changed;
-  }
-
-  PreservedAnalyses run(MachineFunction &MF,
-                        MachineFunctionAnalysisManager &) {
-    bool Changed = runImpl(MF);
-    if (!Changed) {
-      return PreservedAnalyses::all();
-    }
-    return PreservedAnalyses::none();
+    return Changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
   }
 };
-
-class LegacySysliftCollectSyscallsPass : public MachineFunctionPass {
-public:
-  static char ID;
-
-  LegacySysliftCollectSyscallsPass() : MachineFunctionPass(ID) {}
-
-  StringRef getPassName() const override { return "SysliftCollectSyscallsPass"; }
-
-  bool runOnMachineFunction(MachineFunction &MF) override {
-    return SysliftCollectSyscallsPass::runImpl(MF);
-  }
-};
-
-char LegacySysliftCollectSyscallsPass::ID = 0;
-static RegisterPass<LegacySysliftCollectSyscallsPass> LegacyX(
-    "syslift-collect-syscalls",
-    "Collect all AArch64 svc #0 instruction sites into an ELF section", false,
-    false);
 
 } // namespace
 
@@ -297,13 +195,18 @@ extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
       LLVM_VERSION_STRING,
       [](PassBuilder &PB) {
         PB.registerPipelineParsingCallback(
-            [](StringRef Name, MachineFunctionPassManager &MFPM,
+            [](StringRef Name, ModulePassManager &MPM,
                ArrayRef<PassBuilder::PipelineElement>) {
               if (Name != "syslift-collect-syscalls") {
                 return false;
               }
-              MFPM.addPass(SysliftCollectSyscallsPass());
+              MPM.addPass(SysliftCollectSyscallsPass());
               return true;
+            });
+
+        PB.registerOptimizerLastEPCallback(
+            [](ModulePassManager &MPM, OptimizationLevel, ThinOrFullLTOPhase) {
+              MPM.addPass(SysliftCollectSyscallsPass());
             });
       },
   };
