adcf01d296214e9e9e497844bc4bbada25d308f3
ShawnZhong <github@shawnzhong.com>
2026-02-19 04:10:16 -0600

    Remove cc.sh

--- run.sh
+++ run.sh
@@ -22,3 +22,7 @@ run build/loader --deny 172 build/getpid
 run build/write
 run build/loader build/write
 run build/loader --deny 64 build/write
+
+run build/print_pid
+run build/loader build/print_pid
+run build/loader --deny 172 build/print_pid
--- samples/build.mk
+++ samples/build.mk
@@ -2,7 +2,9 @@ SAMPLE_SRCS := $(wildcard samples/*.c)
 SAMPLE_NAMES := $(patsubst samples/%.c,%,$(SAMPLE_SRCS))
 SAMPLE_BINS := $(addprefix $(BUILD_DIR)/,$(SAMPLE_NAMES))
 
-$(BUILD_DIR)/%: samples/%.c cc.sh $(PASS_SO) | $(BUILD_DIR)
-	./cc.sh $< $(BUILD_DIR)/$*
+SAMPLE_CLANG_FLAGS := -O2 -Ithird_party/nolibc -fpass-plugin=$(PASS_SO) -nostdlib -static
+
+$(BUILD_DIR)/%: samples/%.c $(PASS_SO) | $(BUILD_DIR)
+	$(CC) $(SAMPLE_CLANG_FLAGS) $< -o $@
 
 samples: $(SAMPLE_BINS)
