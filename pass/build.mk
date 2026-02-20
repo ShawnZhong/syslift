5b2393ca62651299f942adead71fc11f199fc29a
ShawnZhong <github@shawnzhong.com>
2026-02-20 00:20:17 +0000

    Update

--- pass/build.mk
+++ pass/build.mk
@@ -1,9 +1,8 @@
 PASS_SO := $(BUILD_DIR)/libSysliftCollectSyscallsPass.so
 PASS_SRC := pass/SysliftCollectSyscalls.cpp
 
-PASS_LLVM_CONFIG ?= llvm-config-18
-PASS_LLVM_CXXFLAGS := $(shell $(PASS_LLVM_CONFIG) --cxxflags)
-PASS_LLVM_LDFLAGS := $(shell $(PASS_LLVM_CONFIG) --ldflags --system-libs --libs core passes codegen support)
+PASS_LLVM_CXXFLAGS := $(shell $(LLVM_CONFIG) --cxxflags)
+PASS_LLVM_LDFLAGS := $(shell $(LLVM_CONFIG) --ldflags --system-libs --libs core passes codegen support)
 
 $(PASS_SO): $(PASS_SRC) | $(BUILD_DIR)
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(PASS_LLVM_CXXFLAGS) -shared $< -o $@ $(PASS_LLVM_LDFLAGS)
